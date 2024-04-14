bf606d41131b90e27986f91222da7b8c6e2900cd
zainab shaikh <zeenshaikh93@gmail.com>
2024-04-14 22:22:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    { message: "Select one of each operator to perform action", type: "list", name: "operator", choices: ["+", "-", "/", "*"] },
+]);
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "/") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    (console.log("invalid value"));
